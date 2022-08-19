03b0b36dd833e74fd805fc15df45159c51d1daa7
Lcchy <lcchy@mailbox.org>
2022-08-19 17:43:42 +0100

    Stash

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,26 @@ fn main() -> Result<()> {
     // Define the Jack process (to refactor)
     let jack_process = move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
         let mut out_buff = out_port.writer(ps);
+        // out_buff.max_event_size()
 
-        let midi_msg = RawMidi {
+        // MIDI 90 3C 40 : Ch1 Note on P60 V64
+        let note_on = RawMidi {
             time: ps.frames_since_cycle_start(),
             bytes: &[144, 60, 64],
-        }; // MIDI 90 3C 40 : Ch1 Note on P60 V64
+        };
 
-        out_buff.write(&midi_msg).unwrap();
+        // MIDI 80 3C 40 : Ch1 Note off P60 V64  | vel is arbitrary
+        let note_off = RawMidi {
+            time: ps.frames_since_cycle_start(),
+            bytes: &[128, 60, 64],
+        };
+
+        out_buff.write(&note_on).unwrap();
 
         jack::Control::Continue
     };
 
-    // Start the Jack thread
+    // Start the Jack thread/usr/share/codium/resources/app/out/vs/code/electron-sandbox/workbench/workbench.html
     let process = jack::ClosureProcessHandler::new(jack_process);
     let active_client = jclient.activate_async((), process).unwrap();
 
