e82c4631c55b5c0cf5d986a90d869efbee91b301
Lcchy <lcchy@mailbox.org>
2022-08-23 18:07:31 +0100

    Stash

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,67 @@
 use anyhow::Result;
 use jack::{Client, ClientOptions, RawMidi};
-use std::io;
+use std::{
+    io,
+    sync::{Arc, RwLock},
+};
+
+struct MidiNote {
+    // on_off: bool,
+    /// Channel, should be 0-15
+    channel: u8,
+    pitch: u8,
+    velocity: u8,
+    /// usec
+    len: u64,
+}
+
+impl MidiNote {
+    fn get_raw_note_on_bytes(&self) -> [u8; 3] {
+        [9 * 16 + self.channel, self.pitch, self.velocity]
+    }
+
+    // fn get_raw_note_on<'a, 'b>(&'a self, cycle_frames: u32) -> RawMidi<'b>
+    // where
+    //     'a: 'b,
+    // {
+    //     RawMidi {
+    //         time: cycle_frames,
+    //         bytes: &[9 * 16 + self.channel, self.pitch, self.velocity],
+    //         // bytes: &self.get_raw_note_on_bytes().to_owned(),
+    //     }
+    // }
+}
+
+struct Event {
+    e_type: EventType,
+    /// Quarter note event position on bpm time scale
+    time: u64,
+}
+
+enum EventType {
+    MidiNoteOn(MidiNote),
+    MidiNoteOff(MidiNote),
+}
+
+type EventBuffer = Vec<Event>;
+
+#[derive(Clone)]
+struct Params {
+    /// Current position in the event buffer.
+    /// Write: jack process, Read: -
+    event_head: usize,
+    /// In usecs. To be reset on loop.
+    /// Write: jack process, Read: -
+    curr_time: u64,
+    /// Write: osc process, Read: Jack process
+    bpm: Arc<RwLock<u16>>,
+    /// In quarter notes of bpm. 16 = 1 measure
+    /// Write: osc process, Read: Jack process
+    loop_length: Arc<RwLock<u16>>,
+    /// Write: osc process, Read: Jack process
+    event_buffer: Arc<RwLock<EventBuffer>>,
+    //TODO maybe bundle RwLocks?
+}
 
 fn main() -> Result<()> {
     // Set up jack ports
@@ -10,19 +71,58 @@ fn main() -> Result<()> {
         .register_port("gisele_out", jack::MidiOut::default())
         .unwrap();
 
-    //TODO define a buffer holding midi notes information, shared between jack and osc process
-    // Could be a dynamic vector of Note{midiInfo, len}
-    // Define shared BPM
-    // Use BPM and some current_time in usec to compute if note should be played in current cycle (and at which frame?), start at 1 per BPM
+    //TODO Use BPM and some current_time in usec to compute if note should be played in current cycle (and at which frame?), start at 1 per BPM
     // Print period_time, estimate if cycle is short enough for ms precision of onset
+    // TODO LATER: have a central sequencer process that pushes out events to jack midi or osc sender
 
-    // Define the Jack process (to refactor)
+    // Init values
+    let event_buffer_arc = Arc::new(RwLock::new(vec![
+        // TODO builder function
+        Event {
+            e_type: EventType::MidiNoteOn(MidiNote {
+                channel: 0,
+                pitch: 60,
+                velocity: 64,
+                len: 1_000_000,
+            }),
+            time: 0,
+        },
+        Event {
+            e_type: EventType::MidiNoteOff(MidiNote {
+                channel: 0,
+                pitch: 60,
+                velocity: 64,
+                len: 1_000_000,
+            }),
+            time: 4,
+        },
+    ]));
+    let bpm_arc = Arc::new(RwLock::new(120));
+    let loop_length_arc = Arc::new(RwLock::new(16));
+    let params_arc = Params {
+        event_head: 0,
+        curr_time: 0,
+        event_buffer: event_buffer_arc,
+        bpm: bpm_arc,
+        loop_length: loop_length_arc,
+    };
+    let mut params_ref = params_arc.clone();
+
+    // Define the Jack process
     let jack_process = move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
         let mut out_buff = out_port.writer(ps);
         // out_buff.max_event_size()
 
-        let cy_times = ps.cycle_times();
-        cy_times.unwrap();
+        let cy_times = ps.cycle_times().unwrap();
+
+        let loop_len = params_ref.loop_length.read().unwrap();
+        let next_event_time = params_ref.event_buffer.read().unwrap()[params_ref.event_head].time;
+
+        //TODO switch to event time in usecs, do conversion on insertion
+        if 4 * next_event_time < cy_times.next_usecs % (loop_len.checked_mul(60u16).unwrap() as u64)
+        {
+            params_ref.event_head += 1;
+        }
 
         // MIDI 90 3C 40 : Ch1 Note on P60 V64
         let note_on = RawMidi {
@@ -42,7 +142,7 @@ fn main() -> Result<()> {
         jack::Control::Continue
     };
 
-    // Start the Jack thread/usr/share/codium/resources/app/out/vs/code/electron-sandbox/workbench/workbench.html
+    // Start the Jack thread
     let process = jack::ClosureProcessHandler::new(jack_process);
     let active_client = jclient.activate_async((), process).unwrap();
 
